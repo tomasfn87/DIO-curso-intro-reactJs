0f8f04ca5a372e6d04a2bb82aa0731688b9fd365
tomasfn87 <tomas.nalle@alumni.usp.br>
2021-10-25 16:48:59 -0300

    App React com "webpack-dev-server" rodando.

--- webpack-babel/src/index.js
+++ webpack-babel/src/index.js
@@ -1,1 +1,5 @@
-console.log("React with Webpack and Babel")
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App.js"
+
+ReactDOM.render(<App />, document.getElementById("root"))
